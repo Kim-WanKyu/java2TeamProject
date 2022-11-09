3f65598cfa54ca919b397902b9f396057716a256
Kim-WanKyu <qwertnmagicalmaze@gmail.com>
2022-11-09 19:23:41 +0900

    Book, User 클래스의 변수,생성자,메소드에 한정자 추가

--- src/Book/Book.java
+++ src/Book/Book.java
@@ -1,16 +1,16 @@
 package Book;
 
 public class Book {
-	String name;		//도서 이름
-	String author;		//저자명
-	String publisher;	//출판사명
-	String id;			//ID(ISBN)
-	String category;	//분류(KDC기준 간략하게)
-	int totalCount;		//총 권 수
-	int borrowCount;	//빌린 권 수
+	private String name;		//도서 이름
+	private String author;		//저자명
+	private String publisher;	//출판사명
+	private String id;			//ID(ISBN)
+	private String category;	//분류(KDC기준 간략하게)
+	private int totalCount;		//총 권 수
+	private int borrowCount;	//빌린 권 수
 	
 	//Book 생성자
-	Book(String name, String author, String publisher, String id
+	public Book(String name, String author, String publisher, String id
 			, String category, int totalCount, int borrowCount) {
 		this.name = name;
 		this.author = author;
@@ -22,21 +22,20 @@ public class Book {
 	}
 	
 	//get 메소드들
-	String getName() { return this.name; }
-	String getAuthor() { return this.author; }
-	String getPublisher() { return this.publisher; }
-	String getId() { return this.id; }
-	String getCategory() { return this.category; }
-	int getTotalCount() { return this.totalCount; }
-	int getBorrowCount() { return this.borrowCount; }
+	public String getName() { return this.name; }
+	public String getAuthor() { return this.author; }
+	public String getPublisher() { return this.publisher; }
+	public String getId() { return this.id; }
+	public String getCategory() { return this.category; }
+	public int getTotalCount() { return this.totalCount; }
+	public int getBorrowCount() { return this.borrowCount; }
 
 	//set 메소드들
-	void setName(String name) { this.name = name; }
-	void setAuthor(String author) { this.author = author; }
-	void setPublisher(String publisher) { this.publisher = publisher; }
-	void setId(String id) { this.id = id; }
-	void setCategory(String category) { this.category = category; }
-	void setTotalCount(int totalCount) { this.totalCount = totalCount; }
-	void setBorrowCount(int borrowCount) { this.borrowCount = borrowCount; }
-	
+	public void setName(String name) { this.name = name; }
+	public void setAuthor(String author) { this.author = author; }
+	public void setPublisher(String publisher) { this.publisher = publisher; }
+	public void setId(String id) { this.id = id; }
+	public void setCategory(String category) { this.category = category; }
+	public void setTotalCount(int totalCount) { this.totalCount = totalCount; }
+	public void setBorrowCount(int borrowCount) { this.borrowCount = borrowCount; }
 }
--- src/User/User.java
+++ src/User/User.java
@@ -8,16 +8,16 @@ import Book.Book;	//Book패키지의 Book클래스 import
 
 
 public class User {
-	String name;			//회원 이름
-	String id;				//학번/교번(=ID)
-	String password;		//비밀번호
-	Boolean isAdmin;		//관리자여부
-	Book[] borrowBooks;		//대여한 책(들)
-	LocalDate[] borrowDates;//대여한 날짜(들)
-	String[] delayInfo;		//대여한 책의 연체 정보(들)
+	private String name;			//회원 이름
+	private String id;				//학번/교번(=ID)
+	private String password;		//비밀번호
+	private Boolean isAdmin;		//관리자여부
+	private Book[] borrowBooks;		//대여한 책(들)
+	private LocalDate[] borrowDates;//대여한 날짜(들)
+	private String[] delayInfo;		//대여한 책의 연체 정보(들)
 	
 	//User 생성자
-	User(String name, String id, String password, Boolean isAdmin
+	public User(String name, String id, String password, Boolean isAdmin
 			, Book[] borrowBooks, LocalDate[] borrowDates, String[] delayInfo) {
 		this.name = name;
 		this.id = id;
@@ -29,21 +29,21 @@ public class User {
 	}
 	
 	//get 메소드들
-	String getName() { return this.name; }
-	String getID() { return this.id; }
-	String getPassword() { return this.password; }
-	Boolean getIsAdmin() { return this.isAdmin; }
-	Book[] getBorrowBooks() { return this.borrowBooks; }
-	LocalDate[] getBorrowDates() { return this.borrowDates; }
-	String[] getDelayInfo() { return this.delayInfo; }
+	public String getName() { return this.name; }
+	public String getID() { return this.id; }
+	public String getPassword() { return this.password; }
+	public Boolean getIsAdmin() { return this.isAdmin; }
+	public Book[] getBorrowBooks() { return this.borrowBooks; }
+	public LocalDate[] getBorrowDates() { return this.borrowDates; }
+	public String[] getDelayInfo() { return this.delayInfo; }
 	
 	//set 메소드들
-	void setName(String name) { this.name = name; }
-	void setID(String id) { this.id = id; }
-	void setPassword(String password) { this.password = password; }
-	void setIsAdmin(Boolean isAdmin) { this.isAdmin = isAdmin; }
-	void setBorrowBooks(Book[] borrowBooks) { this.borrowBooks = borrowBooks; }
-	void setBorrowDates(LocalDate[] borrowDates) { this.borrowDates = borrowDates; }
-	void setDelayInfo(String[] delayInfo) { this.delayInfo = delayInfo; }
+	public void setName(String name) { this.name = name; }
+	public void setID(String id) { this.id = id; }
+	public void setPassword(String password) { this.password = password; }
+	public void setIsAdmin(Boolean isAdmin) { this.isAdmin = isAdmin; }
+	public void setBorrowBooks(Book[] borrowBooks) { this.borrowBooks = borrowBooks; }
+	public void setBorrowDates(LocalDate[] borrowDates) { this.borrowDates = borrowDates; }
+	public void setDelayInfo(String[] delayInfo) { this.delayInfo = delayInfo; }
 
 }
