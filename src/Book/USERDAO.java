7b35a4b50c2cd49a08b491e53ab7b23eee7bbb2a
Dongju <wjdehdwn2475@naver.com>
2022-11-10 13:18:37 +0900

    데베 어렵....

--- src/Book/USERDAO.java
+++ src/Book/USERDAO.java
@@ -3,25 +3,24 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.Vector;
 
 import User.User;
 public class USERDAO {
-	public class UserDAO {
+	public static void main(String args[]) throws SQLException {
+		USERDAO user = new USERDAO();
+		user.insertUser();
+	}
 		private static USERDAO user_instance = new USERDAO();
 		public static USERDAO getInstance() {
 			return user_instance;
 		}
 		//모든 책 가져오기
-	public Vector<String> getUser() throws SQLException{
-		Vector<User> dbUserlist = getAllUser();
-		Vector<String> Userlist = new Vector<String>();
-		for(User user : dbUserlist) {
-			Userlist.add(user.getName());
-		}
-		return Userlist;
-	}
-	//모든 책 품목 가져오기
+
+	
+	//모든 유저 가져와 초기화 메서드
 	public Vector<User> getAllUser() throws SQLException{
 		Vector<User>list = new Vector<>();
 		Connection conn = null;
@@ -29,22 +28,23 @@ public class USERDAO {
 		ResultSet rs =null;
 		String price = null;
 		String sql = "select *from User";
+		User user;
 		try
 		{
 			conn = JDBBOOK.connect();
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
 			while (rs.next()) {
-				User user = new User();
+				user = new User();
 				user.setName(rs.getString("Name"));
 				user.setID(rs.getString("id"));
 				user.setuserdata(user.getID(),user.getName());
 				user.setIsAdmin(rs.getBoolean("isAdmin"));
-				user.setBorrowBooks(rs.getString("BorrowbooksandBooks"));
+				user.setBorrowBooksandDates(rs.getString("BorrowbooksandBooks"));
 				
 				//반목문 안의 반복문으로 구현해야함!!
-				user.setUserbook(rs.getString("borrowbooks"), rs.getString("borrowdates"));
 			}
+			
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
@@ -52,13 +52,61 @@ public class USERDAO {
 		}
 		return list;
 	}
-	//연체 리스트 뽑기
-	public void PrintLoanlist() {
-		User user = new User();
-		
+	//빌린 책 그리고 연체 날짜 저장 메서드
+public HashMap<String,String> getborrowbooksanddata(String id)throws SQLException{
+	Connection conn = null;
+	PreparedStatement ptsmt = null;
+	ResultSet rs = null;
+	String BooksandBorrow = null;
+	String sql = "select BorrowandBooks from student where id=? ";
+	User user;
+	HashMap<String,String> BooksandBorrowMap = new HashMap<String,String>();
+		try {
+			conn=JDBBOOK.connect();
+			ptsmt=conn.prepareStatement(sql);
+			ptsmt.setString(1, id);
+			rs = ptsmt.executeQuery();
+			if(rs.next()) {
+				BooksandBorrow = rs.getString("BorrowbooksAndBorrowdates");
+				System.out.println(BooksandBorrow);
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			JDBBOOK.close();
+		}
+		return BooksandBorrowMap;
 	}
 	
-	//책 품목 추가
-
+	//유저 추가
+	public boolean insertUser() {
+		Connection conn =null;
+		PreparedStatement pstmt = null;
+		String sql = "INSERT INTO student('Name','id','password','isAdmin','Borrowbooks','delayInfo','Studentcol')VALUES(?,?,?,?,?,?,?)";
+		boolean result = false;
+		try {
+		 conn=JDBBOOK.connect();
+		 pstmt = conn.prepareStatement(sql);
+		 pstmt.setString(1, "정동주");
+		 pstmt.setString(2, "202001826");
+		 pstmt.setString(3,"jdj2001@@");
+		 pstmt.setInt(4, 1);
+		 pstmt.setString(5,"오만과 편견 11:59");
+		 pstmt.setString(6,"sadasdasdas");
+		 pstmt.setString(7,"asdasdad");
+		 System.out.println("삽입 성공");
+//		 int r = pstmt.executeUpdate();
+// 		System.out.println("return result = "+r);
+//		 if(r>0) {
+//			 result = true;
+//		 }
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+		JDBBOOK.close();
+	}
+	return result;
 }
 }
--- src/User/User.java
+++ src/User/User.java
@@ -1,6 +1,7 @@
 package User;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.TreeMap;
 import java.time.LocalDate;
 
@@ -18,21 +19,21 @@ public class User {
 	//아이디와 비밀번호를 트리맵에 넣음 로그인과 정보갱신등에 사용할 거임
 	static private TreeMap<String,String> userdata = new TreeMap<String,String>();
 	//연체 여부도 트리맵으로 구성!!
-	private TreeMap<String,String> userbook = new TreeMap<String,String>();
+//	private HashMap<String,String> userbook = new HashMap<String,String>();
 	//대여한 책의 연체 정보(들)
 	//user 해쉬 맵써야 함
 	//jsonArrray 사용
 	//User 생성자
 	
-	public User( String name,  String id, String password, boolean isAdmin, String borrowBooksandDates, String borrowDates, String delayInfo) {
-		this.name = name;
-		this.id = id;
-		this.password = password;
-		this.isAdmin = isAdmin;
-		this.borrowBooksandDates = borrowBooksandDates;
-		this.borrowDates = borrowDates;
-		this.delayInfo = delayInfo;
-	}
+//	public User( String name,  String id, String password, boolean isAdmin, String borrowBooksandDates, String borrowDates, String delayInfo) {
+//		this.name = name;
+//		this.id = id;
+//		this.password = password;
+//		this.isAdmin = isAdmin;
+//		this.borrowBooksandDates = borrowBooksandDates;
+//		this.borrowDates = borrowDates;
+//		this.delayInfo = delayInfo;
+//	}
 	public User(){}
 	
 	//get 메소드들
@@ -40,7 +41,7 @@ public class User {
 	public	 String getID() { return this.id; }
 	public	 String getPassword() { return this.password; }
 	public	Boolean getIsAdmin() { return this.isAdmin; }
-	String getBorrowBooks() { return this.borrowBooksandDates; }
+	String getBorrowBooksandDates() { return this.borrowBooksandDates; }
 	String getBorrowDates() { return this.borrowDates; }
     String getDelayInfo() { return this.delayInfo; }
 	
@@ -49,9 +50,11 @@ public class User {
 	public void setID(String id) { this.id = id; }
 	public void setPassword(String password) { this.password = password; }
 	public void setIsAdmin(Boolean isAdmin) { this.isAdmin = isAdmin; }
-	public void setBorrowBooks(String borrowBooks) { this.borrowBooksandDates = borrowBooks; }
+	public void setBorrowBooksandDates(String borrowBooks) { this.borrowBooksandDates = borrowBooks; }
 	public void setBorrowDates(String borrowDates) { this.borrowDates = borrowDates; }
 	public void setDelayInfo(String delayInfo) { this.delayInfo = delayInfo; }
 	public void setuserdata(String id,String pass) {userdata.put(id,pass);}
 	public void setUserbook(String book,String bookborrowdates) {userbook.put(book, bookborrowdates);}
+	//빌린 책 그리고 연체 날짜 저장메서드 
+	
 }
