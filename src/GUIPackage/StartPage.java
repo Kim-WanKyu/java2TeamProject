c3268212b171c2227096e9df5ff9ddb4964f1946
Kim-WanKyu <qwertnmagicalmaze@gmail.com>
2022-11-22 17:44:00 +0900

    초기화면 클래스 DB로그인 제거

--- src/GUIPackage/StartPage.java
+++ src/GUIPackage/StartPage.java
@@ -116,61 +116,38 @@ public class StartPage extends Page{
 	public void actionPerformed(ActionEvent ae) {
 		switch(ae.getActionCommand()) {
 		case "로그인":
-			try {
-				String id = idTextField.getText(); //입력한 id
-				String password = new String(pwPasswordField.getPassword()); //입력한 pw
-				
-				dispose();		//초기화면 창 끄고
-				new MainPage(); //메인화면 창 생성
-				
-				UserManager loguser = UserManager.getInstance();
-				loguser.setAllUser();
-				System.out.println("유저 세팅 완료");
-<<<<<<< HEAD
-				boolean isuser = loguser.Login(id, password);
-				
-				//로그인 메소드 추가 필요
-				//if(boolean 로그인 메소드(id, pw) == true) //성공
-				//{
-				//	dispose();		//초기화면 창 끄고
-				//	dispose();		//초기화면 창 끄고
-				//	new MainPage(user);	//메인화면 창 생성 / 추후에 MainPage클래스 생성자에 (user정보) 추가 필요
-				//}
-				//else
-				//{
-				//	//메시지창(경고) 띄우는 메소드
-				//	MessageBox.printWarningMessageBox("입력한 정보가 없거나, \n비밀번호가 일치하지 않습니다.");
-				//}
-				
-				//TODO 완성하고 정리할 것 괄호 들여쓰기도 할 것.
-				//setuser setting
-				
-				//임시로 지정
-				if(id.equals("123") && password.equals("123"))
-
-				{
-					//user가 맞는지 확인 if(user's_id & id's_pw isIn userDB)
-					
-					//켜져있는 모든 창 끄기
-					for(int i=0; i<getOwnerlessWindows().length;i++)
-						getOwnerlessWindows()[i].dispose();
-					new MainPage();	//메인화면 창 생성 / 추후에 MainPage클래스 생성자에 (user정보) 추가 필요
-				}
-				//else 아니면 에러 메세지
-				else
-				{
-					//메시지창(경고) 띄우는 메소드
-					MessageBox.printWarningMessageBox("입력한 정보가 없거나, \n비밀번호가 일치하지 않습니다.");
-				}
-			}catch(Exception e) {
-				
-			}
+			String id = idTextField.getText(); //입력한 id
+			String password = new String(pwPasswordField.getPassword()); //입력한 pw
 			
-=======
-				boolean isuser = loguser.Login(idTextField.getText(),pwPasswordField.getText());
+			dispose();		//초기화면 창 끄고
+			//new MainPage(); //메인화면 창 생성
+			
+			//TODO
+			//UserManager loguser = UserManager.getInstance();
+			//loguser.setAllUser();
+			//System.out.println("유저 세팅 완료");
+			
+			//TODO
+			////boolean isuser = loguser.Login(id, password);
+			
+			//로그인 메소드 추가 필요
+			//if(boolean 로그인 메소드(id, pw) == true) //성공
+			//{
+			//	dispose();		//초기화면 창 끄고
+			//	dispose();		//초기화면 창 끄고
+			//	new MainPage(user);	//메인화면 창 생성 / 추후에 MainPage클래스 생성자에 (user정보) 추가 필요
+			//}
+			//else
+			//{
+			//	//메시지창(경고) 띄우는 메소드
+			//	MessageBox.printWarningMessageBox("입력한 정보가 없거나, \n비밀번호가 일치하지 않습니다.");
+			//}
+			
+			//TODO 완성하고 정리할 것 괄호 들여쓰기도 할 것.
+			//setuser setting
+			
 			//임시로 지정
 			if(id.equals("123") && password.equals("123"))
-
 			{
 				//user가 맞는지 확인 if(user's_id & id's_pw isIn userDB)
 				
@@ -185,8 +162,6 @@ public class StartPage extends Page{
 				//메시지창(경고) 띄우는 메소드
 				MessageBox.printWarningMessageBox("입력한 정보가 없거나, \n비밀번호가 일치하지 않습니다.");
 			}
-			}catch(Exception e) {}
->>>>>>> 70d31f37f88f1c102f9930f1e4689eee88039c8b
 			break;
 			
 		case "회원등록":
