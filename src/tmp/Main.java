687979b448b06c641f861658fdd8af1a9d6cfea1
Kim-WanKyu <qwertnmagicalmaze@gmail.com>
2022-11-30 23:20:46 +0900

    패키지 임시 지정

---
+++ src/tmp/Main.java
@@ -0,0 +1,13 @@
+package tmp;
+
+import gui.page.startPage.StartPage;
+
+public class Main {
+	
+	//main 메소드
+	public static void main(String args[]) {
+
+		//초기화면 실행
+		new StartPage();
+	}
+}
---
+++ src/tmp/login.java
@@ -0,0 +1,21 @@
+package tmp;
+
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import Book.Book;
+import Book.BookManager;
+
+public class login {
+	public static void main(String[] args) throws SQLException {
+			User user = new User();
+			UserManager loguser = new UserManager().getInstance();
+			BookManager BookDB = new BookManager().getInstance();
+			BookDB.setAllBook();
+			Book a = new Book();
+			user.setID("201801826");
+			a.setId("9788901243658");
+			loguser.borrowBooks(user, a);
+		}
+}
